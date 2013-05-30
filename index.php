d506d994f7b6a0b03884007776abaabefa0e4619
twipsy <twipsy@git.(none)>
2013-05-30 12:13:16 +0200

    afegida resta

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
             $c=$a+$b;
             return $c;
         }
-        
+
+	function resta($a,$b){
+	   $c=$a+$b;
+	   return $c;
+	}
+
         suma(5,234);
-        
+	resta (234,5);
+
         ?>
     </body>
 </html>
